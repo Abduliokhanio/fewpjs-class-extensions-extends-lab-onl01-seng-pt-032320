11c0d8a0b45210f89935422b45acd446876d0ceb
Abdul Khan <akhan17@outlook.com>
2020-10-01 15:04:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,3 +24,42 @@ class Polygon {
     }
 
 }
+class Triangle extends Polygon {
+  // constructor(array){
+  //   // this.sides = array
+  // }
+    get isValid(){
+      if( this.count !== 3 ) return
+      let side1 = this.sides[0]
+      let side2 = this.sides[1]
+      let side3 = this.sides[2]
+        return side1 < side2 + side3 && side2 < side1 +side3 && side3< side1 +side2
+
+      // we inherit countSides from Polygon class
+      // next we see if the sides in this shape = 3?
+    }
+
+
+}
+
+class Square extends Polygon {
+
+  get isValid(){
+    if (this.count !== 4) return
+    let side1 = this.sides[0]
+    let side2 = this.sides[1]
+    let side3 = this.sides[2]
+    let side4 = this.sides[3]
+      return side1 === side2 && side2 ===side3 && side3 === side4
+
+  }
+
+  get area(){
+    if (this.count !== 4) return
+    //
+    return  this.sides[0] * this.sides[1]
+
+}
+
+
+}
