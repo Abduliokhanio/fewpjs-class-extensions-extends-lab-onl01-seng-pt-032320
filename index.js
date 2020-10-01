b0ae6919e048a2cd5475d600b8432e1cf1152524
Abdul Khan <akhan17@outlook.com>
2020-10-01 15:04:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+class Polygon {
+  constructor(array){
+  this.sides = array
+  this.count = array.length
+    // initializer for this class
+    // array -> represents the sides of the this polygon
+  }
+    get countSides(){
+    return this.sides.length
+
+    }
+
+    get perimeter(){
+      let sum = 0
+      for ( let i = 0; i < this.sides.length; i++) {
+
+        sum = sum + this.sides[i]
+        // total equals the side of each array
+        // sum = 0  + index[0] etc...
+      }
+
+      return sum
+    }
+
+}
